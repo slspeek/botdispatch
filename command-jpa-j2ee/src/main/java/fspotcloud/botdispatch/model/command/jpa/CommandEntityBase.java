43a4b20403884a69e15888902f06c8a5b3be74d8
Steven L. Speek <slspeek@gmail.com>
2012-02-09 23:38:20 +0100

    0.32-bd works with FSC 0.11-dev-12

---
+++ command-jpa-j2ee/src/main/java/fspotcloud/botdispatch/model/command/jpa/CommandEntityBase.java
@@ -0,0 +1,49 @@
+package fspotcloud.botdispatch.model.command.jpa;
+
+import fspotcloud.botdispatch.model.api.Command;
+import java.util.Date;
+import javax.persistence.*;
+
+@Entity
+public abstract class CommandEntityBase implements Command {
+
+    @Id
+    //@GeneratedValue(generator="increment")
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //Derby with Hibernate do not want this, I thought
+    //@GenericGenerator(name = "increment", strategy = "increment")
+    private Long id;
+    @Temporal(javax.persistence.TemporalType.TIMESTAMP)
+    private Date ctime;
+    @Basic
+    private boolean locked;
+
+    public CommandEntityBase() {
+        ctime = new Date();
+    }
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public Date getCtime() {
+        return ctime;
+    }
+
+    @Override
+    public String toString() {
+        String result = " : ";
+        return result;
+    }
+
+    @Override
+    public void setLocked(boolean locked) {
+        this.locked = locked;
+    }
+
+    @Override
+    public boolean isLocked() {
+        return locked;
+    }
+}
