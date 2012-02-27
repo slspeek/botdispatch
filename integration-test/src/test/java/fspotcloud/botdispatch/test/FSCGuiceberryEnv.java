ede2dedfdd76e73ac0cd5251113ec6d08e1616b8
Steven L. Speek <slspeek@gmail.com>
2012-02-27 19:20:47 +0100

    HtmlUnitDriver for selenium tests (for CI)

--- integration-test/src/test/java/fspotcloud/botdispatch/test/FSCGuiceberryEnv.java
+++ integration-test/src/test/java/fspotcloud/botdispatch/test/FSCGuiceberryEnv.java
@@ -9,13 +9,15 @@ import com.google.guiceberry.TestScoped;
 import com.google.guiceberry.TestWrapper;
 import com.google.inject.Provides;
 import com.thoughtworks.selenium.Selenium;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class FSCGuiceberryEnv extends GuiceBerryModule {
 
 	@Provides
 	@TestScoped
 	Selenium getSelenium() {
-		WebDriver driver = new FirefoxDriver();
+		HtmlUnitDriver driver = new HtmlUnitDriver();//new FirefoxDriver();
+                driver.setJavascriptEnabled(true);
 		return new WebDriverBackedSelenium(driver, "http://localhost:8080");
 	}
 
