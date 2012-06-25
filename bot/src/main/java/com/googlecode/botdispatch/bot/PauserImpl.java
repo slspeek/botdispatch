1ceb6568a9bb0bb42787914f4f072e9a62e3b21c
Steven L. Speek <slspeek@gmail.com>
2012-06-25 22:46:17 +0200

    Better logging and listens to keyboard to accelarate

--- bot/src/main/java/com/googlecode/botdispatch/bot/PauserImpl.java
+++ bot/src/main/java/com/googlecode/botdispatch/bot/PauserImpl.java
@@ -3,11 +3,18 @@ package com.googlecode.botdispatch.bot;
 import com.google.inject.name.Named;
 
 import javax.inject.Inject;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import static java.lang.System.*;
 
 public class PauserImpl implements Pauser {
 
+    public static final int MILLIS_IN_SECOND = 1000;
     private int idleCount = 0;
     final private int maxSeconds;
+    private static final int PERIOD = 20;
 
     @Inject
     public PauserImpl(@Named("pause") int maxSeconds) {
@@ -17,10 +24,28 @@ public class PauserImpl implements Pauser {
 
     public int pause() {
         int seconds = getPauseSeconds();
+        long millis = seconds * MILLIS_IN_SECOND;
+        String line = null;
+        long endTime = currentTimeMillis() + millis;
         try {
-            Thread.sleep(seconds * 1000);
-        } catch (InterruptedException e) {
+            while (currentTimeMillis() < endTime ) {
+                 if ( in.available() != 0) {
+                         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                         line = br.readLine();
+                                 resetIdleCount();
+                                 break;
+                 }
+                try {
+                    Thread.sleep(PERIOD);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
+
+
         return seconds;
     }
 
